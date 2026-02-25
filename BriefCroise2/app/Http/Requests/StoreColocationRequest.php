09ac5a128cc012f8c6b1c0565a89364db2834d03
a-oirgari <abdrahmanghali1998@gmail.com>
2026-02-25 20:31:30 +0000

    chore: the store methode for colocationRequest

--- BriefCroise2/app/Http/Requests/StoreColocationRequest.php
+++ BriefCroise2/app/Http/Requests/StoreColocationRequest.php
@@ -6,23 +6,23 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreColocationRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Le nom de la colocation est obligatoire.',
+            'name.max'      => 'Le nom ne peut pas dépasser 255 caractères.',
         ];
     }
 }
