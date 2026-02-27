bc2d8ea5f16057997bfe544945daa4238318ab5f
a-oirgari <abdrahmanghali1998@gmail.com>
2026-02-27 20:43:02 +0000

    chore: the routes of the project manually

--- BriefCroise2/routes/web.php
+++ BriefCroise2/routes/web.php
@@ -1,8 +1,73 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ColocationController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', fn() => redirect()->route('login'));
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::get('/invitations/{token}', [InvitationController::class, 'show'])->name('invitations.show');
+
+
+Route::middleware(['auth.custom'])->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    
+    Route::get('/profile', [AuthController::class, 'showProfile'])->name('profile');
+    Route::post('/profile', [AuthController::class, 'updateProfile'])->name('profile.update');
+
+    
+    Route::get('/colocations', [ColocationController::class, 'index'])->name('colocations.index');
+    Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocations.create');
+    Route::post('/colocations', [ColocationController::class, 'store'])->name('colocations.store');
+    Route::get('/colocations/{colocation}', [ColocationController::class, 'show'])->name('colocations.show');
+    Route::get('/colocations/{colocation}/edit', [ColocationController::class, 'edit'])->name('colocations.edit');
+    Route::put('/colocations/{colocation}', [ColocationController::class, 'update'])->name('colocations.update');
+    Route::post('/colocations/{colocation}/cancel', [ColocationController::class, 'cancel'])->name('colocations.cancel');
+    Route::post('/colocations/{colocation}/leave', [ColocationController::class, 'leave'])->name('colocations.leave');
+    Route::delete('/colocations/{colocation}/members/{member}', [ColocationController::class, 'removeMember'])->name('colocations.members.remove');
+
+    
+    Route::post('/colocations/{colocation}/invitations', [InvitationController::class, 'store'])->name('invitations.store');
+    Route::post('/invitations/{token}/accept', [InvitationController::class, 'accept'])->name('invitations.accept');
+    Route::post('/invitations/{token}/refuse', [InvitationController::class, 'refuse'])->name('invitations.refuse');
+
+    
+    Route::post('/colocations/{colocation}/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+    Route::delete('/colocations/{colocation}/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+
+    
+    Route::get('/colocations/{colocation}/payments', [PaymentController::class, 'index'])->name('payments.index');
+    Route::post('/colocations/{colocation}/payments', [PaymentController::class, 'store'])->name('payments.store');
+
+    
+    Route::get('/colocations/{colocation}/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::post('/colocations/{colocation}/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::delete('/colocations/{colocation}/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+    
+    Route::middleware(['admin'])->prefix('admin')->name('admin.')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+        Route::get('/users', [AdminController::class, 'users'])->name('users');
+        Route::post('/users/{user}/ban', [AdminController::class, 'ban'])->name('users.ban');
+        Route::post('/users/{user}/unban', [AdminController::class, 'unban'])->name('users.unban');
+    });
 });
-
